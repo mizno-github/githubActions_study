95579d041c0379bf32ee42ac620c45bd5480eaf8
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-07 12:47:15 +0900

    パスの調整

--- bash_test_dir/donb/1_bash.bash
+++ bash_test_dir/donb/1_bash.bash
@@ -1,4 +1,4 @@
-env_tree_ignore="^config.json$|^\.$|directories"
+env_tree_ignore="^config.json$|^\.$|directories|^bash_test_dir/donb/$"
 # configファイル
 # カレントディレクトリ(.のみ)
 # 結果テキスト（「0 directories, 2 files」）を除外
@@ -9,7 +9,7 @@ env_tree_ignore="^config.json$|^\.$|directories"
 # export LANG=en_US.UTF-8
 
 # ０から９から始まらないファイルを抽出
-treeError=$(tree -Ii "[0-9]_*|[0-9][0-9]_*" bash_test_dir/donb/ | grep -vE $env_tree_ignore)
+treeError=$(tree -Ii "[0-9]_*|[0-9][0-9]_*" ./bash_test_dir/donb/ | grep -vE $env_tree_ignore)
 
 if [ -n "$treeError" ] ; then
   echo '数字から始まらないファイルが存在します'
