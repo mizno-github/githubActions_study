eed56e3ebd3181efc1a7a1329380ceed8725d346
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-07 10:55:31 +0900

    yumコマンドの消去

--- bash_test_dir/donb/1_bash.bash
+++ bash_test_dir/donb/1_bash.bash
@@ -3,10 +3,10 @@ env_tree_ignore="^config.json$|^\.$|directories"
 # カレントディレクトリ(.のみ)
 # 結果テキスト（「0 directories, 2 files」）を除外
 
-yum -y install tree
+# yum -y install tree
 
 # 多分ここは必要ない
-export LANG=en_US.UTF-8
+# export LANG=en_US.UTF-8
 
 # ０から９から始まらないファイルを抽出
 treeError=$(tree -Ii "[0-9]_*|[0-9][0-9]_*" | grep -vE $env_tree_ignore)
