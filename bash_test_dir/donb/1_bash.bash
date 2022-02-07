a6a4c0947efd4cb7c33bf113acdc7014d898ed2b
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-07 15:51:51 +0900

    test

--- bash_test_dir/donb/1_bash.bash
+++ bash_test_dir/donb/1_bash.bash
@@ -1,4 +1,4 @@
-env_tree_ignore="^[0-9]_*|[0-9][0-9]_*|config.json$|^\.$|directories"
+env_tree_ignore="^[0-9]_*|[0-9][0-9]_*|config.json$|^\.$|directories|^content.md$|^index.html$|^style.css$|^index.js$|^images$|^lessons$"
 # configファイル
 # カレントディレクトリ(.のみ)
 # 結果テキスト（「0 directories, 2 files」）を除外
@@ -8,6 +8,7 @@ env_tree_ignore="^[0-9]_*|[0-9][0-9]_*|config.json$|^\.$|directories"
 # 多分ここは必要ない
 # export LANG=en_US.UTF-8
 cd bash_test_dir/donb/
+touch config.json
 # ０から９から始まらないファイルを抽出
 treeError=$(tree -i | grep -vE $env_tree_ignore)
 
