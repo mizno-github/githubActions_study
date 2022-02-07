b609561633a5ca8e5f73eff306cad50ffe0b0303
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-07 10:37:34 +0900

    数字から始まらないファイルのエラーテスト

---
+++ bash_test_dir/donbler/1_bash.bash
@@ -0,0 +1,17 @@
+env_tree_ignore="^config.json$|^\.$|directories"
+# configファイル
+# カレントディレクトリ(.のみ)
+# 結果テキスト（「0 directories, 2 files」）を除外
+
+yum -y install tree
+
+# 多分ここは必要ない
+export LANG=en_US.UTF-8
+
+# ０から９から始まらないファイルを抽出
+treeError=$(tree -Ii "[0-9]_*|[0-9][0-9]_*" | grep -vE $env_tree_ignore)
+
+if [ -n "$treeError" ] ; then
+  echo '数字から始まらないファイルが存在します'
+  echo "$treeError "
+fi
