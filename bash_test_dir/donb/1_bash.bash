6e24d8dc1dc2f393bdbe828e53821470439ca280
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-07 14:27:37 +0900

    パスの確認

--- bash_test_dir/donb/1_bash.bash
+++ bash_test_dir/donb/1_bash.bash
@@ -9,21 +9,21 @@ env_tree_ignore="^[0-9]_*|[0-9][0-9]_*|config.json$|^\.$|directories"
 # export LANG=en_US.UTF-8
 pws
 ls
-cd bash_test_dir/donb/
-# ０から９から始まらないファイルを抽出
-treeError=$(tree -i | grep -vE $env_tree_ignore)
+# cd bash_test_dir/donb/
+# # ０から９から始まらないファイルを抽出
+# treeError=$(tree -i | grep -vE $env_tree_ignore)
 
 
 
-MESSAGE="ここがプルリクのタイトルになる
+# MESSAGE="ここがプルリクのタイトルになる
 
-## これ以降が説明欄の文章になる
-* テスト
-* test
-markdown で書ける。
-if [ -n "$treeError" ] ; then
-  数字から始まらないファイルが存在します
-  $treeError
-fi
-"
-hub pull-request -m "$MESSAGE" -b main -h dev
+# ## これ以降が説明欄の文章になる
+# * テスト
+# * test
+# markdown で書ける。
+# if [ -n "$treeError" ] ; then
+#   数字から始まらないファイルが存在します
+#   $treeError
+# fi
+# "
+# hub pull-request -m "$MESSAGE" -b main -h dev
