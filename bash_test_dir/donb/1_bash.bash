bf5a8ede5b81919742af366223a83768745db6ab
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-08 14:40:03 +0900

    jsファイルの作成

--- bash_test_dir/donb/1_bash.bash
+++ bash_test_dir/donb/1_bash.bash
@@ -3,12 +3,9 @@ env_tree_ignore="^[0-9]_*|[0-9][0-9]_*|config.json$|^\.$|directories|^content.md
 # カレントディレクトリ(.のみ)
 # 結果テキスト（「0 directories, 2 files」）を除外
 
-# yum -y install tree
 
-# 多分ここは必要ない
-# export LANG=en_US.UTF-8
 cd bash_test_dir/donb/
-touch config.json
+
 # ０から９から始まらないファイルを抽出
 treeError=$(tree -i | grep -vE $env_tree_ignore)
 # --noreport tree -J
@@ -17,12 +14,31 @@ if [ -n "$treeError" ] ; then
   echo $treeError
 fi
 
-touch workfile.json
 
-# tree -JP "[0-9]_*" >>  makeJson.js
+################     ファイル名の一部抽出&Json化    ##################
 
-# sed -i './workfile.json' makeJson.js
+# gitの変更履歴で1_front or 2_backを選択する
+workdir=1_front
 
-# sed -i 'beforeJson = ' makeJson.js
+# 対象のディレクトリに移動
+cd $workdir/lessons
 
-# cd ../../
+touch ../config.json
+
+# 1_frontをedit.txtに入れる
+echo var workdir = "$workdir" >> edit.txt
+
+# gitの変更履歴の部分で「1_html/cssを学ぼう」の部分を選択する
+editFile=1_html／cssを学ぼう
+
+# 編集したディレクトリの配下をJsonとしてedit.txtに保存
+tree -JP "[0-9]_*" $editFile  --noreport >>  edit.txt
+
+# edit.txtの中身をmakeJson.jsの先頭に追加する
+sed -i 'edit.txt' ../../makeJson.js
+
+# makeJson.jsを実行し、config.jsに保存する
+node ../../makeJson.js >> ../config.json
+
+# 作業ディレクトリに戻る
+cd ../../
--- bash_test_dir/donb/makeJson.js
+++ bash_test_dir/donb/makeJson.js
@@ -1,24 +1,13 @@
-var afterJson = {
-  "courseName": "",
-  "lessonInfo": [
-  {
-    "lessonName": "",
-    "chapterName": []
-  }
-  ]
-}
 
-for(var i = 0;i <beforeJson.length-1;i++){
-    afterJson.courseName = beforeJson[i]['name'];
-    var lessonInfos = beforeJson[i]['contents'];
-    for(var n=0;n < lessonInfos.length; n++){
-        afterJson.lessonInfo[i].lessonName = lessonInfos[n]['name'];
-        var chapters = lessonInfos[n]['contents'];
-        if(chapters){
-            for(var c = 0; c < chapters.length; c++){
-                afterJson.lessonInfo[i].chapterName.push(chapters[c]);
-            }
-        }
+var afterJson = {}
+afterJson.courseName = workdir;
+afterJson.lessonInfo = {};
+for(var n = 0; n < beforeJson.length; n++){
+    afterJson.lessonInfo.chapterName = [{}]
+    var beforeChapter = beforeJson[n]['contents']
+    for(var i = 0; i < beforeChapter.length; i++){
+        afterJson.lessonInfo.chapterName[i] = beforeChapter[i].name;
     }
 }
 
+console.log(JSON.stringify(afterJson, null, 2));
